35ed9896252c18b3e089acec74b1e5bb4011eaa4
田阳阳 <1004994051@qq.com>
2022-01-19 17:06:05 +0800

    feat:添加token拦截器异常的情况下进行提示

--- lib/network/token_interceptor.dart
+++ lib/network/token_interceptor.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_github/res/constant_data.dart';
 import 'package:flutter_github/utils/log_util.dart';
 import 'package:flutter_github/utils/shared_preferences_util.dart';
+import 'package:oktoast/oktoast.dart';
 
 /// @auter Created by tyy on 2022/1/19
 /// desc   :token拦截器
@@ -23,8 +24,17 @@ class TokenInterceptor extends QueuedInterceptorsWrapper {
       options.headers["Authorization"] = "token $token";
     } else {
       LogUtils.d("token is .......null");
+      showToast("去登录");
     }
 
     super.onRequest(options, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    LogUtils.d(
+        "Token出现错误${err.type}   ${err.response?.statusCode}      ${err.message}");
+    showToast("去登录，试试");
+    super.onError(err, handler);
   }
 }
