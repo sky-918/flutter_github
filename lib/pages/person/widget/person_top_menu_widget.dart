7aba306162fc71f98555ea2d8b1571900cbfd124
田阳阳 <1004994051@qq.com>
2022-01-20 15:23:15 +0800

    feat:个人中心tab整理

--- lib/pages/person/widget/person_top_menu_widget.dart
+++ lib/pages/person/widget/person_top_menu_widget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../pages/person/bloc/person_bloc.dart';
 import '../../../res/res_index.dart';
 
 /// @auter Created by tyy on 2022/1/4
@@ -13,27 +15,29 @@ class PersonTopMenuWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: AppColors.appColor,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          _getMenuItem(),
-          _getMenuItem(),
-          _getMenuItem(),
-          _getMenuItem(),
-          _getMenuItem(),
-        ],
-      ),
+      child: BlocBuilder<PersonBloc, PersonState>(builder: (context, state) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _getMenuItem(AppStrings.homeWidgetMenuRepos,state.personBean.publicRepos.toString()),
+            _getMenuItem(AppStrings.homeWidgetMenuFans,state.personBean.followers.toString()),
+            _getMenuItem(AppStrings.homeWidgetMenuFocus,"0"),
+            _getMenuItem(AppStrings.homeWidgetMenuStart,"0"),
+            _getMenuItem(AppStrings.homeWidgetMenuHonor,"0"),
+          ],
+        );
+      }),
     );
   }
 
-  _getMenuItem() {
+  _getMenuItem(String name, String num) {
     return Expanded(
         flex: 1,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text("title"), Text("data")],
+          children: [Text(name), Text(num)],
         ));
   }
 }
--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -6,5 +6,11 @@ class AppStrings {
   static const String appTitle = "GitHub";
   static const String homeWidgetMenuDynamic = "动态";
   static const String homeWidgetMenuTrend = "趋势";
-  static const String homeWidgetMenuPerson = "趋势";
+  static const String homeWidgetMenuPerson = "我的";
+  static const String homeWidgetMenuRepos = "仓库";
+  static const String homeWidgetMenuFans = "粉丝";
+  static const String homeWidgetMenuFocus = "关注";
+  static const String homeWidgetMenuStart = "星标";
+  static const String homeWidgetMenuHonor = "荣耀";
+
 }
