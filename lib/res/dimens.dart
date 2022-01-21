c1872567b041e6c72ff5d62685daad8ec839f5cb
田阳阳 <1004994051@qq.com>
2022-01-21 10:10:23 +0800

    feat:修改字体大小

--- lib/res/dimens.dart
+++ lib/res/dimens.dart
@@ -1,11 +1,13 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 /// @auter Created by tyy on 2021/12/28
 /// desc   :
 /// version:v1.0.0
 ///
 class AppDimens{
-  static const double font_18sp = 18.0;
-  static const double font_16sp = 16.0;
-  static const double font_14sp = 14.0;
-  static const double font_12sp = 12.0;
-  static const double font_10sp = 10.0;
+  static  double font_18sp = 18.0.sp;
+  static  double font_16sp = 16.0.sp;
+  static  double font_14sp = 14.0.sp;
+  static  double font_12sp = 12.0.sp;
+  static  double font_10sp = 10.0.sp;
 }
--- lib/res/text_style.dart
+++ lib/res/text_style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'dimens.dart';
 
@@ -8,27 +9,27 @@ import 'dimens.dart';
 ///
 class TextStyles {
   ///一级标题
-  static const TextStyle textSize18Bold = TextStyle(
+  static  TextStyle textSize18Bold = TextStyle(
       fontSize: AppDimens.font_18sp,
       fontWeight: FontWeight.bold,
       fontStyle: FontStyle.normal);
 
-  static const TextStyle textSize16 = TextStyle(
+  static  TextStyle textSize16 = TextStyle(
       fontSize: AppDimens.font_16sp,
       fontWeight: FontWeight.normal,
       fontStyle: FontStyle.normal);
 
-  static const TextStyle textSize14 = TextStyle(
+  static  TextStyle textSize14 = TextStyle(
       fontSize: AppDimens.font_14sp,
       fontWeight: FontWeight.normal,
       fontStyle: FontStyle.normal);
 
-  static const TextStyle textSize12 = TextStyle(
+  static  TextStyle textSize12 = TextStyle(
       fontSize: AppDimens.font_12sp,
       fontWeight: FontWeight.normal,
       fontStyle: FontStyle.normal);
 
-  static const TextStyle textSize10 = TextStyle(
+  static  TextStyle textSize10 = TextStyle(
       fontSize: AppDimens.font_10sp,
       fontWeight: FontWeight.normal,
       fontStyle: FontStyle.normal);
